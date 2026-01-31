aeb4fad2a450d28cca6c941633b0ef0d0005dcdd
Kevin Ramberg <kevin.ramberg@popmenu.com>
2026-01-31 16:00:22 -0500

    add range validations

--- app/models/account.rb
+++ app/models/account.rb
@@ -4,4 +4,17 @@ class Account < ApplicationRecord
 
   validates :name, presence: true
   validates :base_service_fee, presence: true, numericality: { greater_than: 0 }
+  validates :minimum_bookable_time, presence: true, numericality: { greater_than: 0 }
+  validates :maximum_bookable_time, presence: true, numericality: { greater_than: 0 }
+  validate :maximum_bookable_time_greater_than_or_equal_to_minimum
+
+  private
+
+  def maximum_bookable_time_greater_than_or_equal_to_minimum
+    return if maximum_bookable_time.blank? || minimum_bookable_time.blank?
+
+    if maximum_bookable_time < minimum_bookable_time
+      errors.add(:maximum_bookable_time, "must be greater than or equal to minimum bookable time")
+    end
+  end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,6 +3,39 @@ class Booking < ApplicationRecord
 
   validates :pet_type, presence: true
   validates :expected_fee, presence: true, numericality: { greater_than: 0 }
-  validates :time_span, presence: true, numericality: { greater_than: 0 }
+  validate :time_span_within_account_bookable_range
   validates :date_of_service, presence: true
+  validate :date_of_service_does_not_conflict_with_other_bookings
+
+  private
+
+  def time_span_within_account_bookable_range
+    return if time_span.blank? || account.blank?
+
+    min = account.minimum_bookable_time
+    max = account.maximum_bookable_time
+    return if min.blank? || max.blank?
+
+    allowed_max = max - min
+    if time_span > allowed_max
+      errors.add(:time_span, "must be less than or equal to the account's bookable time range (#{allowed_max})")
+    end
+  end
+
+  def date_of_service_does_not_conflict_with_other_bookings
+    return if date_of_service.blank? || time_span.blank? || account_id.blank?
+
+    start_time = date_of_service
+    end_time = date_of_service + time_span.minutes
+
+    conflicting = account.bookings.where.not(id: id).any? do |other|
+      other_start = other.date_of_service
+      other_end = other.date_of_service + other.time_span.minutes
+      start_time < other_end && other_start < end_time
+    end
+
+    if conflicting
+      errors.add(:date_of_service, "cannot overlap with another booking for this account")
+    end
+  end
 end
