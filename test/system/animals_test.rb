cdef99a941fbc62a8ede535944e8c944d2eecffa
Kevin Ramberg <kevin.ramberg@popmenu.com>
2026-02-01 17:17:13 -0500

    update form views tests

--- test/system/animals_test.rb
+++ test/system/animals_test.rb
@@ -2,36 +2,61 @@ require "application_system_test_case"
 
 class AnimalsTest < ApplicationSystemTestCase
   setup do
+    @account = accounts(:one)
     @animal = animals(:one)
   end
 
   test "visiting the index" do
-    visit animals_url
-    assert_selector "h1", text: "Animals"
+    visit account_animals_url(@account)
+    assert_selector "h1", text: /Animals/
   end
 
-  test "should create animal" do
-    visit animals_url
-    click_on "New animal"
+  test "animal form creates animal when valid" do
+    visit account_animals_url(@account)
+    click_on "Create new animal"
 
-    fill_in "Additional hour fee", with: @animal.additional_hour_fee
-    fill_in "Name", with: @animal.name
+    assert_selector "h1", text: "New Animal"
+    fill_in "Name", with: "Rex"
+    fill_in "Additional hour fee", with: "12.50"
     click_on "Create Animal"
 
     assert_text "Animal was successfully created"
-    click_on "Back"
+    assert_current_path account_animals_path(@account)
+    assert_text "Rex"
   end
 
-  test "should update Animal" do
+  test "animal form shows validation errors when invalid" do
+    visit new_account_animal_url(@account)
+
+    fill_in "Name", with: ""
+    fill_in "Additional hour fee", with: ""
+    click_on "Create Animal"
+
+    assert_text "error"
+    assert_selector "h1", text: "New Animal"
+  end
+
+  test "animal form updates animal when valid" do
     visit animal_url(@animal)
     click_on "Edit this animal", match: :first
 
-    fill_in "Additional hour fee", with: @animal.additional_hour_fee
-    fill_in "Name", with: @animal.name
+    assert_selector "h1", text: /Editing/
+    fill_in "Name", with: "Updated Pet"
+    fill_in "Additional hour fee", with: "15.00"
     click_on "Update Animal"
 
     assert_text "Animal was successfully updated"
-    click_on "Back"
+    assert_text "Updated Pet"
+  end
+
+  test "animal form shows validation errors on edit when invalid" do
+    visit edit_animal_url(@animal)
+
+    fill_in "Name", with: ""
+    click_on "Update Animal"
+
+    assert_text "error"
+    assert_selector "h1", text: /Editing/
   end
 
   test "should destroy Animal" do
--- test/system/bookings_test.rb
+++ test/system/bookings_test.rb
@@ -2,46 +2,85 @@ require "application_system_test_case"
 
 class BookingsTest < ApplicationSystemTestCase
   setup do
+    @account = accounts(:one)
     @booking = bookings(:one)
+    @animal = animals(:one)
+    # Time that doesn't overlap fixtures (one: 13:23-14:23, two: 15:23-16:23)
+    @non_overlapping_datetime = Time.zone.parse("2026-01-31 17:23:00")
   end
 
   test "visiting the index" do
-    visit bookings_url
-    assert_selector "h1", text: "Bookings"
+    visit account_bookings_url(@account)
+    assert_selector "h1", text: /Upcoming Bookings/
   end
 
-  test "should create booking" do
-    visit bookings_url
-    click_on "New booking"
+  test "booking form creates booking when valid" do
+    visit account_bookings_url(@account)
+    click_on "Create new booking"
 
-    fill_in "Date of service", with: @booking.date_of_service
-    fill_in "Expected fee", with: @booking.expected_fee
-    fill_in "First name", with: @booking.first_name
-    fill_in "Last name", with: @booking.last_name
-    fill_in "Pet name", with: @booking.pet_name
-    fill_in "Pet type", with: @booking.pet_type
-    fill_in "Time span", with: @booking.time_span
+    assert_selector "h1", text: "New booking"
+    fill_in "First name", with: "Jane"
+    fill_in "Last name", with: "Doe"
+    fill_in "Pet name", with: "Rex"
+    select @animal.name, from: "Pet type"
+    fill_in "Time span", with: "2"
+    fill_in "Date of service", with: @non_overlapping_datetime.strftime("%Y-%m-%dT%H:%M")
     click_on "Create Booking"
 
     assert_text "Booking was successfully created"
-    click_on "Back"
+    assert_current_path account_bookings_path(@account)
+    assert_text "Jane"
   end
 
-  test "should update Booking" do
+  test "booking form shows validation errors when invalid" do
+    visit new_account_booking_url(@account)
+
+    fill_in "First name", with: ""
+    fill_in "Last name", with: ""
+    select "Select an animal", from: "Pet type"
+    click_on "Create Booking"
+
+    assert_text "error"
+    assert_selector "h1", text: "New booking"
+  end
+
+  test "booking form shows validation error when date overlaps" do
+    visit new_account_booking_url(@account)
+
+    fill_in "First name", with: "Jane"
+    fill_in "Last name", with: "Doe"
+    fill_in "Pet name", with: "Rex"
+    select @animal.name, from: "Pet type"
+    fill_in "Time span", with: "2"
+    # Overlap with booking one (13:23, time_span 1 -> 13:23-14:23)
+    fill_in "Date of service", with: Time.zone.parse("2026-01-31 13:30:00").strftime("%Y-%m-%dT%H:%M")
+    click_on "Create Booking"
+
+    assert_text "error"
+    assert_selector "h1", text: "New booking"
+  end
+
+  test "booking form updates booking when valid" do
     visit booking_url(@booking)
     click_on "Edit this booking", match: :first
 
-    fill_in "Date of service", with: @booking.date_of_service
-    fill_in "Expected fee", with: @booking.expected_fee
-    fill_in "First name", with: @booking.first_name
-    fill_in "Last name", with: @booking.last_name
-    fill_in "Pet name", with: @booking.pet_name
-    fill_in "Pet type", with: @booking.pet_type
-    fill_in "Time span", with: @booking.time_span
+    assert_selector "h1", text: "Editing Booking"
+    fill_in "First name", with: "Updated"
+    fill_in "Last name", with: "Client"
     click_on "Update Booking"
 
     assert_text "Booking was successfully updated"
-    click_on "Back"
+    assert_text "Updated"
+  end
+
+  test "booking form shows validation errors on edit when invalid" do
+    visit edit_booking_url(@booking)
+
+    fill_in "First name", with: ""
+    click_on "Update Booking"
+
+    assert_text "error"
+    assert_selector "h1", text: "Editing Booking"
   end
 
   test "should destroy Booking" do
